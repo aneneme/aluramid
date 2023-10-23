fe4600b2108f4d3e1ec83b93e793caf11712f9b1
aneneme <148064049+aneneme@users.noreply.github.com>
2023-10-23 08:36:53 -0300

    .

--- aluramidi-curso-aula2/main.js
+++ aluramidi-curso-aula2/main.js
@@ -1,9 +1,13 @@
-function tocaSomPom () {
-    document.querySelector('#som_tecla_pom').play();
+function tocaSomPom (idElementoAudio) {
+    document.querySelector(idElementoAudio).play();
 }
 
-const listadeTeclas = document.querySelectorAll('.tecla');
+const listadeTeclas = document.querySelectorAll('.tecla'),let contador = 0 ;
 
-while  () {
-    listadeTeclas[0].onclick = tocaSomPom;
+while  (contador<listadeTeclas.lenght) {
+    listaDeTeclas[contador].classList[1];
+    listadeTeclas[contador].onclick = function () {
+        tocaSom('#som_tecla_splash');
+    };
+    contador = contador +1;
 }
